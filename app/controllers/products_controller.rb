e03b872a0e12d1cf3ce89610251917894a1c058a
authorbeard <msnolan@gmail.com>
2016-05-08 18:57:25 -0400

    all tests passing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,15 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def show
+    @product = Product.find(params[:id])
+# byebug
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @product.to_json(only: [:id, :name, :description, :inventory, :price]) }
+    end
+  end
+
   def inventory
     product = Product.find(params[:id])
     render plain: product.inventory > 0 ? true : false
@@ -22,14 +31,12 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
-  def show
-    @product = Product.find(params[:id])
-  end
 
-  def data
-    product = Product.find(params[:id])
-    render json: ProductSerializer.serialize(product)
-  end
+
+  # def data
+  #   product = Product.find(params[:id])
+  #   render json: ProductSerializer.serialize(product)
+  # end
 
   private
 
