6ee195952f3796a83b5d710972b83d518ef666a8
Chase Paules <cpaules@gmail.com>
2017-11-09 21:17:55 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
-
-10.times do
-  products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
-  Product.create(:name => products.sample, :price => Faker::Number.between(1,1500))
-end
-5.times do
-  Customer.create(:name => Faker::Name.name)
-end
-
-5.times do
-  Invoice.create
-end
-10.times do
-  order = Order.create(:customer_id => Customer.all.collect(&:id).sample, :invoice_id => Invoice.all.collect(&:id).sample)
-  order.products << Product.find(Product.all.collect(&:id).sample)
-end
-
+p1 = Product.create!(name: "Test Product", inventory: 0, description: "This is a test description with more text than should be there.", price: "2.99")
+p2 = Product.create!(name: "Test Product 2", inventory: 1, description: "This is a second test description with more text than should be there.", price: "1.99")
