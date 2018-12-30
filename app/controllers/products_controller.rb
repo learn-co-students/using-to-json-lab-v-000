4d75fb2e378324ccae3f0ba700577a55575fab7f
Carol Greene <carolgreene3@gmail.com>
2018-12-30 19:27:59 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,11 +24,15 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @product.to_json(only: [:name, :price, :description, :inventory, :id])}
+    end
   end
 
-  def data
+  def data                 #don't really need this anymore bc show is handling both html & json
     product = Product.find(params[:id])
-    render json: ProductSerializer.serialize(product)
+    render json: product.to_json(only: [:name, :price, :description, :inventory, :id])
   end
 
   private
