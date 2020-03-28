f0c564d37420108affa168d9d1c38e748da81194
RonSala <ron.sala@gmail.com>
2020-03-28 16:11:22 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@ products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "la
 
 10.times do
   products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
+  # Product.create(:name => products.sample, :price => Faker::Number.between(from: 1,to: 1500))
   Product.create(:name => products.sample, :price => Faker::Number.between(from: 1,to: 1500))
 end
 5.times do
@@ -22,4 +23,3 @@ end
   order = Order.create(:customer_id => Customer.all.collect(&:id).sample, :invoice_id => Invoice.all.collect(&:id).sample)
   order.products << Product.find(Product.all.collect(&:id).sample)
 end
-
