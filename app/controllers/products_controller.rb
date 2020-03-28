64ca4222f3d1c6d3ad89a9d09a80286fe0346243
RonSala <ron.sala@gmail.com>
2020-03-28 16:13:40 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -34,7 +34,6 @@ class ProductsController < ApplicationController
 
   def data
     product = Product.find(params[:id])
-    # render json: ProductSerializer.serialize(product)
     render json: product.to_json(only: %i[id name description inventory])
   end
 
